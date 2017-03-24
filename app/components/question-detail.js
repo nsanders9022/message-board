db2a3db642f4c161d57ec64f54638ebf532bc8ef
Nicole Sanders <nsanders9022@gmail.com>
2017-03-24 11:05:24 -0700

    add update feature to questions

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-  delete(question) {
+    delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
       }
-    }
+    },
   }
 });
---
+++ app/components/update-question.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        note: this.get('note')
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+      author: this.set('author', '');
+      content: this.set('content', '');
+      note: this.set('note', '');
+    }
+  }
+});
